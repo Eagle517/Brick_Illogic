618b38cb2eb6e06e43f48795f57a45c0da4e6163
Eagle517 <15026800+Eagle517@users.noreply.github.com>
2018-02-25 05:49:09 -0600

    Added left-side output AND

--- bricks/AND.cs
+++ bricks/AND.cs
@@ -5,7 +5,19 @@ datablock fxDTSBrickData(Logic1x2fANDData : Logic1x2fORData)
 	logicUIDesc = "C is true if A and B are true";
 };
 
+datablock fxDTSBrickData(Logic1x2fANDLData : Logic1x2fANDData)
+{
+	brickFile = "add-ons/brick_illogic/bricks/blb/1x2f_2i_1oL.blb";
+	uiName = "AND Left";
+	logicPortPos[2] = "0 1 0";
+};
+
 function Logic1x2fANDData::doLogic(%this, %obj)
+{
+	%obj.Logic_SetOutput(2, $LBC::Ports::BrickState[%obj, 0] && $LBC::Ports::BrickState[%obj, 1]);
+}
+
+function Logic1x2fANDLData::doLogic(%this, %obj)
 {
 	%obj.Logic_SetOutput(2, $LBC::Ports::BrickState[%obj, 0] && $LBC::Ports::BrickState[%obj, 1]);
 }
