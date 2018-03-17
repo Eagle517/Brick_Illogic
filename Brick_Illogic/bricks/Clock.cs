a482786d3f7e0ceceb8de422d898212c4e63db75
Eagle517 <15026800+Eagle517@users.noreply.github.com>
2018-03-16 19:25:07 -0500

    Finished clock

--- Brick_Illogic/bricks/Clock.cs
+++ Brick_Illogic/bricks/Clock.cs
@@ -14,18 +14,18 @@ datablock fxDTSBrickData(LogicGate_Clock_Data)
 	isLogicInput = false;
 
 	logicUIName = "Clock";
-	logicUIDesc = "Uses brick name for half clock cycle. Format is LogicClock_TIME in milliseconds";
+	logicUIDesc = "Uses brick name for half clock cycle. Format is LogicClock_TIME";
 
 	numLogicPorts = 2;
 
 	logicPortType[0] = 1;
 	logicPortPos[0] = "0 0 0";
-	logicPortDir[0] = 3;
+	logicPortDir[0] = 0;
 	logicPortUIName[0] = "Enable";
 
 	logicPortType[1] = 0;
 	logicPortPos[1] = "0 0 0";
-	logicPortDir[1] = 1;
+	logicPortDir[1] = 2;
 	logicPortUIName[1] = "Output";
 };
 
@@ -37,16 +37,32 @@ function LogicGate_Clock_Data::doLogic(%this, %obj)
 		%obj.LBCSched = %this.schedule(%obj.logicClockSpeed, "doLogic", %obj);
 		%obj.Logic_SetOutput(1, !$LBC::Ports::BrickState[%obj, 1]);
 	}
+	else
+		%obj.Logic_SetOutput(1, false);
+}
+
+function LogicGate_Clock_Data::Logic_onGateAdded(%this, %obj)
+{
+	if(%obj.logicClockSpeed < 8)
+		%obj.setName("_LogicClock_8");
 }
 
 package Illogic_Clock
 {
 	function fxDTSBrick::setName(%this, %name)
 	{
-		if(%this.getDatablock().getName() $= "LogicGate_Clock_Data")
+		if((%data = %this.getDatablock()).getName() $= "LogicGate_Clock_Data")
 		{
-			talk(%name);
+			%rname = getSubStr(%name, 1, strlen(%name));
+			if(stripos(%rname, "LogicClock") == 0)
+			{
+				%time = mClamp(getWord(strReplace(%rname, "_", " "), 1), 8, 999999);
+				%this.logicClockSpeed = %time;
+				%name = "_LogicClock_"@%time;
+				%data.doLogic(%this);
+			}
 		}
+		parent::setName(%this, %name);
 	}
 };
 activatePackage("Illogic_Clock");
