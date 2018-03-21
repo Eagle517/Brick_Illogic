3704beb12733b94be165e146d2644412974b8f5d
Auios <LoneAuios@gmail.com>
2018-03-21 13:12:03 -0400

    Forgot bricks again

--- Brick_Illogic/bricks.cs
+++ Brick_Illogic/bricks.cs
@@ -133,6 +133,7 @@ exec("./bricks/8bitAdder.cs");
 	//Subtraction
 exec("./bricks/HalfSubtractor.cs");
 exec("./bricks/FullSubtractor.cs");
+exec("./bricks/8bitSubtractor.cs");
 
 	//Multiplication
 exec("./bricks/8bitMultiplier.cs");
