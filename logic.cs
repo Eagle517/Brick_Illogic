04a4ea9d71513d1b5849f7276c5aea197ff659b5
Eagle517 <15026800+Eagle517@users.noreply.github.com>
2018-03-31 03:52:10 -0500

    Fixed UI issue

--- logic.cs
+++ logic.cs
@@ -287,7 +287,7 @@ package IllogicLogic
 							{
 								%states = trim(%states);
 								%gateDesc = %data.logicUIDesc;
-								%client.centerPrint("<font:consolas:20>\c6"@%gateName NL "<color:ffffff>"@%gateDesc NL "\c0"@%states, 3);
+								%client.centerPrint("<font:consolas:20>\c6"@%gateName NL "<color:ffffff>"@%gateDesc NL "<color:ff0000>"@%states, 3);
 								return;
 							}
 							
