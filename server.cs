9429e97a85a0bfe99bc8f25ecd4d8fdcf05a5c11
Auios <LoneAuios@gmail.com>
2018-08-18 02:16:27 -0400

    Changed minimum tick time
    Changed minimum clamp for time from 0 to 1 due to weird jittery behavior noticed when hosting non dedicated server

--- server.cs
+++ server.cs
@@ -37,7 +37,7 @@ function serverCmdLST(%client, %time)
 {
 	if(%client.isAdmin || %client.isSuperAdmin)
 	{
-		$LBC::Opts::Time = mClamp(%time, 0, 999999);
+		$LBC::Opts::Time = mClamp(%time, 1, 999999); //Weird jittery behavior when set to 0
 		messageAll('', '\c3%1\c6 has set the logic tick time to \c3%2\c6 millisecond%3.', %client.name, $LBC::Opts::Time, %time == 1 ? "":"s");
 	}
 }
